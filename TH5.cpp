8ccdea8e6e2053f57227fa36249f5b65f20fcdc0
RZY0517 <62455653+RZY0517@users.noreply.github.com>
2022-01-08 20:05:41 +0800

    Add files via upload

--- TH5.cpp
+++ TH5.cpp
@@ -1,9 +1,41 @@
+//7.2----------------输入两个整数，要求输出其中值较大者。要求用函数来找到大数。
+//#include<stdio.h>
+//#include<Windows.h>
+//int main() {
+//	int MAX(int x, int y);
+//	int a,b,z;
+//	printf("Please enter two numbers\n");
+//	scanf_s("%d %d", &a, &b);
+//	z= MAX(a, b);
+//	printf("max=%d\n", z);
+//	system("pause");
+//	return 0;
+//}
+//int MAX(int x, int y)
+//{
+//	int c;
+//	c = x > y ? x : y;
+//	return c;
+//}
+
+
+
 #include<stdio.h>
 #include<Windows.h>
 int main() {
-	printf("Hello World\n");
+	int MAX(int x, int y);
+	int a, b, z;
+	printf("Please enter two numbers:\n");
+	scanf_s("%d %d", &a, &b);
+	z = MAX(a, b);
+	printf("MAX=%d\n", z);
 
 	system("pause");
-
 	return 0;
+}
+int MAX(int x, int y)
+{
+	int z;
+	z = x > y ? x : y;
+	return z;      //或者直接写成（return （x>y?x:y）);
 }
