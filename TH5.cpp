bf0129187bc4558f096b588bafa34dbca49f0c6f
RZY0517 <62455653+RZY0517@users.noreply.github.com>
2022-01-09 17:13:15 +0800

    Add files via upload

--- TH5.cpp
+++ TH5.cpp
@@ -20,17 +20,52 @@
 
 
 
+//#include<stdio.h>
+//#include<Windows.h>
+//#include<test1.h>    //test1头文件中包含了MAX函数和ppp函数
+//int main() {
+//	//int MAX(int x, int y);
+//	int a, b, z;
+//	printf("Please enter two numbers:\n");
+//	scanf_s("%d %d", &a, &b);
+//	z = MAX(a, b);
+//	printf("MAX=%d\n", z);
+//	ppp();
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//例7.4----------------输入两个实数，用一个函数求出他们之和。
+
+//#include<stdio.h>
+//#include<Windows.h>
+//#include<test1.h>
+//int main() {
+//	float ad = 0;
+//	float a, b;
+//	printf("Please enter two numbers\n");
+//	scanf_s("%f %f", &a, &b);
+//	ad = add(a, b);
+//	printf("%.2f+%.2f=%.2f", a, b, ad);
+//	 
+//	system("pause");
+//	return 0;
+//}
+
+
+//例7.5----------------输入四个数，找出最大的数。利用函数的嵌套来实现。
 #include<stdio.h>
 #include<Windows.h>
-#include<test1.h>    //test1头文件中包含了MAX函数和ppp函数
-int main() {
-	//int MAX(int x, int y);
-	int a, b, z;
-	printf("Please enter two numbers:\n");
-	scanf_s("%d %d", &a, &b);
-	z = MAX(a, b);
-	printf("MAX=%d\n", z);
-	ppp();
+#include<test1.h>
+int main()
+{
+	int a, b, c, d, e, MAX;
+	printf("Please enter five numbers\n");
+	scanf_s("%d %d %d %d %d", &a, &b, &c, &d,&e);
+	MAX = MAX5(a, b, c, d,e);
+	printf("max=%d\n", MAX);
 	system("pause");
 	return 0;
 }
--- test1.h
+++ test1.h
@@ -1,13 +1,49 @@
 #pragma once
-void ppp()
+void ppp()//例7.2
 {
 	printf("this is a test\n");
 }
 
-int MAX(int x, int y)
+int MAX(int x, int y)//例7.2
 {
 	int z;
 	z = x > y ? x : y;
 	return z;      //或者直接写成（return （x>y?x:y）);
 }
 
+
+float add(float x, float y)//例7.4
+{
+	float z;
+	z = x + y;
+	return z;
+}
+
+int MAX4(int a, int b, int c, int d)
+{
+	int MAX2(int x, int y);
+	int m;
+	m = MAX2(a, b);
+	m = MAX2(m, c);
+	m = MAX2(m, d);
+
+	return m;
+}
+
+int MAX2(int x, int y)
+{
+	return (x > y ? x : y);
+}
+
+int MAX5(int a, int b, int c, int d, int e)
+{
+	int MAX2(int x, int y);//以下2种方式简化，省略变量t
+	/*int t;
+	t = MAX2(a, b);
+	t = MAX2(t, c);
+	t = MAX2(t, d);
+	t = MAX2(t, e);
+	return t;*/
+	//return (MAX2(MAX2(MAX2((a, b), c), d), e));//不建议使用
+	return MAX2(MAX2(MAX2(MAX(a, b), c), d), e);
+}
